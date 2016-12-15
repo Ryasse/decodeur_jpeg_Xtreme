2f84a367f4ced7ad884fad7ac2898f4b4633670e
bestsofware <bestsofware@outlook.fr>
2016-12-15 16:13:23 +0100

    prog i/o fichiers

--- file_io.c
+++ file_io.c
@@ -1,5 +1,4 @@
 #include "file_io.h"
-#include <assert.h>
 
 uint8_t* fileRead( const char* filename, int* size )
 {
@@ -18,4 +17,22 @@ uint8_t* fileRead( const char* filename, int* size )
     fclose( stream );
 
     return data;
+}
+
+void imageWritePPM( const char* filename, int width, int height, uint8_t* r, uint8_t* g, uint8_t* b )
+{
+    FILE* stream = fopen( filename, "w" );
+
+    fprintf( stream, "P3\n%d %d\n", width, height );
+
+    int x, y;
+    for( y = 0; y < height; y++ )
+    {
+        for( x = 0; x < width; x++ )
+            fprintf( stream, "%d\t%d\t%d\t\t", r[ y * width + x ], g[ y * width + x ], b[ y * width + x ] );
+
+        fprintf( stream, "\n" );
+    }
+
+    fclose( stream );
 }
--- file_io.h
+++ file_io.h
@@ -12,15 +12,15 @@
  * \brief La structure struct PPM permet de stocker les informations
  * d'entête pour le format PPM.
  **/
-struct PPM{
+typedef struct{
 	// PPM header
 	char magic[2];	/*!< 2 bytes - 'P' '3' */
 	int width;      /*!< 4 bytes : the image width */
 	int height;		/*!< 4 bytes : the image height */
 	int max;		/*!< 4 bytes : max value (less than 65536 and more than zero).*/
-};
+} PPM;
 
 uint8_t* fileRead( const char*, int* );
-void imageWritePPM( char*, int, int, uint8_t*, uint8_t*, uint8_t* );
+void imageWritePPM( const char*, int, int, uint8_t*, uint8_t*, uint8_t* );
 
 #endif /* FILE_IO_H_ */
